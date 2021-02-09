ed385343e4c07e5a50e68706585caa10b35f461e
darrengp <ceo.darrenperry@gmail.com>
2021-02-09 10:52:02 -0800

    comp movement

--- main.js
+++ main.js
@@ -1,23 +1,79 @@
+//// variables
 const container = document.getElementById("container");
+let pixel = "";
+let play = true;
 
 let gameboardArr = [];
 
+///// grid creater
 createGrid = (number) => {
   for (let i = 0; i < number; i++) {
     const div = document.createElement("div");
-    div.style.backgroundColor = "white";
-    div.style.border = "orange solid .01px";
-    div.style.width = "123px";
-    div.style.height = "118px";
-    div.style.margin = "none";
     div.setAttribute("class", "pixel");
-    // div.addEventListener("mousedown", cb);
+    div.setAttribute("id", `pixel${i}`);
+    div.addEventListener("click", onTap);
     container.append(div);
-    const pixel = document.querySelectorAll(".pixel");
+    // pixel = document.querySelectorAll(".pixel");
     gameboardArr.push(div);
-    // console.log(container);
   }
 };
 
 createGrid(20);
+
+//// event listeners
+
+// for (let i = 0; i <= gameboardArr.length; i++) {
+//   if (gameboardArr[i] == undefined) {
+//     console.log(gameboardArr[i]);
+//   } else {
+//     gameboardArr[i].addEventListener("click", onTap);
+//   }
+// }
 console.log(gameboardArr);
+
+console.log("tesstttt");
+
+/// temp computer board levels
+function compPlayB1() {
+  gameboardArr[14].innerText = "🍣";
+  setTimeout(function () {
+    gameboardArr[10].innerText = "🍣";
+    gameboardArr[14].innerText = " ";
+  }, 2000);
+  setTimeout(function () {
+    gameboardArr[6].innerText = "🍣";
+    gameboardArr[10].innerText = " ";
+  }, 2000 * 2);
+  setTimeout(function () {
+    gameboardArr[2].innerText = "🍣";
+    gameboardArr[6].innerText = " ";
+  }, 2000 * 3);
+  setTimeout(function () {
+    gameboardArr[2].innerText = "🍣";
+    gameboardArr[6].innerText = " ";
+  }, 2000 * 4);
+  setTimeout(function () {
+    gameboardArr[2].innerText = " ";
+    alert("players turn! I hope you payed attention");
+  }, 2000 * 4.5);
+}
+compPlayB1();
+
+////////////////////////////////////////////
+
+/// temp onTap // still exploring
+///// onTap function --------------------------
+function onTap(event) {
+  // console.log(box1.value);
+  let a = document.getElementById(event.target.id);
+  // console.log(a.id);
+
+  if (play == false) {
+    console.log("game over");
+    // startState();
+  } else {
+    if (a.id == "pixel10") {
+      console.log(a.id);
+    }
+  }
+}
